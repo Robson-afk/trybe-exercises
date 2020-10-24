0c156bcd9fae153e7e3e8042de3acca950c40583
robson widnef <robsonbatistawidnef2@gmail.com>
2020-10-24 20:48:42 -0300

    :sparkles: Terminado Atividade 3

---
+++ Introduçao_Fundamentos_Web/Bloco_4/Exercicio_3.js
@@ -0,0 +1,14 @@
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+if(a > b && a > c) {
+  console.log("'a' é o maior dos três números");
+}
+else if(b > a && b > c) {
+  console.log("'b' é o maior dos três números");
+}
+else {
+  console.log("'c' é o maior dos três números");
+}
