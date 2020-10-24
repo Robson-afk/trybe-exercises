330e5505dd9be8dad43b037b861eba5d766f6835
robson widnef <robsonbatistawidnef2@gmail.com>
2020-10-24 19:46:16 -0300

    :sparkles: Terminado Atividade 1

--- Introduçao_Fundamentos_Web/Bloco_4/Exercicio_1.js
+++ Introduçao_Fundamentos_Web/Bloco_4/Exercicio_1.js
@@ -1,2 +1,29 @@
 /* ADIÇÃO */
+let a = 11;
+let b = 14;
+resultado = a + b;
+console.log(resultado);
 
+/* SUBTRAÇÃO */
+let a = 11;
+let b = 14;
+resultado = a - b;
+console.log(resultado);
+
+/* MULTIPLICAÇÃO */
+let a = 11;
+let b = 14;
+resultado = a * b;
+console.log(resultado);
+
+/* DIVISÃO */ 
+let a = 11;
+let b = 14;
+resultado = a / b;
+console.log(resultado);
+
+/* MÓDULO */
+let a = 4;
+let b = 4;
+resultado = a % b;
+console.log(resultado);
