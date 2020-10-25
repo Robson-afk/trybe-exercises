eb0cfaf39573c9ea6ea0078102f2f2d5bf9ee3c5
robson widnef <robsonbatistawidnef2@gmail.com>
2020-10-24 21:44:38 -0300

    :sparkles: Terminado Atividade 8

---
+++ Introduçao_Fundamentos_Web/Bloco_4/Exercicio_8.js
@@ -0,0 +1,11 @@
+
+const numberA = 4;
+const numberB = 7;
+const numberC = 12;
+
+if(numberA % 2 === 0 || numberB % 2 === 0 || numberC % 2 === 0){
+  console.log("true");
+}
+else{
+  console.log("false");
+}
