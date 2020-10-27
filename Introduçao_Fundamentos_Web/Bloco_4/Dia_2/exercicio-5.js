e0b01c132ca24855289015a3df85d875bfccec55
robson widnef <robsonbatistawidnef2@gmail.com>
2020-10-26 21:35:25 -0300

    :sparkles: Terminado Exercicio 5

---
+++ Introduçao_Fundamentos_Web/Bloco_4/Dia_2/exercicio-5.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior = 0;
+
+for(let i = 0; i < numbers.length; i++){
+  if(numbers[i] > maior){
+    maior = numbers[i];
+  }
+}
+console.log(`Maior Valor do Array ${maior}`);
