bfa0cd136418a49441edd1a414493ba7fc9f8f45
robson widnef <robsonbatistawidnef2@gmail.com>
2020-11-02 17:27:40 -0300

    :sparkles: Finalizado Exercicio 2

--- Introduçao_Fundamentos_Web/Bloco_5/Dia_1/script.js
+++ Introduçao_Fundamentos_Web/Bloco_5/Dia_1/script.js
@@ -1,2 +1,6 @@
 document.getElementsByTagName('p')[1].innerText =
   'Daqui à 2 anos me vejo como um Programador FrontEnd | React.';
+
+  document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76, 164, 109)";
+
+  
