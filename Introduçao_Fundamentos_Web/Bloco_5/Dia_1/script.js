b455947979d9a465a99d9821513821f91febadf8
robson widnef <robsonbatistawidnef2@gmail.com>
2020-11-02 17:34:20 -0300

    :sparkles: Finalizado Exercicio 3

--- Introduçao_Fundamentos_Web/Bloco_5/Dia_1/script.js
+++ Introduçao_Fundamentos_Web/Bloco_5/Dia_1/script.js
@@ -3,4 +3,5 @@ document.getElementsByTagName('p')[1].innerText =
 
   document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76, 164, 109)";
 
-  
+  document.getElementsByClassName("center-content")[0].style.backgroundColor = "#fff";
+
