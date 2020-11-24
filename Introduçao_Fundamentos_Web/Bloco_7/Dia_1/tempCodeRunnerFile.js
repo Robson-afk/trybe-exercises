ef27de239d8f34820a83fbc8254380e832160b2f
robson widnef <robsonbatistawidnef2@gmail.com>
2020-11-24 15:14:43 -0300

    :sparkles: Finalizado Exercicio 4

--- Introduçao_Fundamentos_Web/Bloco_7/Dia_1/tempCodeRunnerFile.js
+++ Introduçao_Fundamentos_Web/Bloco_7/Dia_1/tempCodeRunnerFile.js
@@ -1,8 +1,11 @@
-const fatorial = (numero) => {
-  let result = 1;
-  for(let index = 2; index <= numero; index += 1) {
-    result *= index;
+const longestWord = (frase) => {
+  let maiorPalavra = '';
+for(let index = 0; index <= frase.length; index += 1) {
+  if(frase[index] > maiorPalavra) {
+    maiorPalavra += frase[index];
   }
-  return result;
 }
-  console.log(fatorial(4));
+return maiorPalavra;
+}
+
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
