d716466d8e0b0b6c7ef70665110f84f4ee58ee77
robson widnef <robsonbatistawidnef2@gmail.com>
2020-11-23 21:33:06 -0300

    :sparkles: Exercicios do Dia 2

---
+++ Introduçao_Fundamentos_Web/Bloco_4/Dia_1/Exercicio_10.js
@@ -0,0 +1,11 @@
+
+let custoProduto = 1;
+let valorVenda = 3;
+
+if (custoProduto >= 0 && valorVenda >= 0) {
+  let totalCustoProduto = custoProduto * 1.2;
+  let total = (valorVenda - totalCustoProduto) * 1000;
+  console.log(total);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
---
+++ Introduçao_Fundamentos_Web/Bloco_4/Dia_1/Exercicio_5.js
@@ -0,0 +1,12 @@
+
+let anguloA = 60;
+let anguloB = 150;
+let anguloC = 35;
+
+let soma = anguloA + anguloB + anguloC;
+
+if (soma === 180) {
+  console.log('Ângulos válidos!');
+} else {
+  console.log('Ângulos válidos!');
+};
