d25c931796c5c52c111211c24fc0a84deda567ff
robson widnef <robsonbatistawidnef2@gmail.com>
2020-12-04 15:26:54 -0300

    :sparkles: Finalizando Dia 2 com Algumas Correçoes

--- Introduçao_Fundamentos_Web/Bloco_5/Dia_2/script.js
+++ Introduçao_Fundamentos_Web/Bloco_5/Dia_2/script.js
@@ -3,6 +3,7 @@ const titulo = document.createElement('h1');
 titulo.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(titulo);
 
+
 // 2.Adicione a tag div com a classe main-content como filho da tag body ;
 const div1 = document.createElement('div');
 div1.classList.add('main-content');
@@ -48,4 +49,32 @@ for (let index in array) {
 }
   lista.style.listStyle = 'none';
 
+  // 9.Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
+  for (let index = 1; index < 4; index += 1) {
+    const div5 = document.createElement('h3');
+    div5.innerText = 'consegui criar!' + index;
+    div1.appendChild(div5);
+  }
 
+// 10.Adicione a classe title na tag h1 criada;
+titulo.classList.add('title');
+
+// 11.Adicione a classe description nas 3 tags h3 criadas;
+  const title3 = document.getElementsByTagName('h3');
+    for (let index = 0; index < 3; index += 1) {
+      title3[index].className = 'description';
+    }
+
+// 12.Remova o div criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+const removeDiv3 = document.getElementsByClassName('left-content')[0];
+div1.removeChild(removeDiv3);
+
+// 13.Centralize o div criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto do div ;
+div4.style.margin = 'auto';
+
+// 14.Troque a cor de fundo do elemento pai da div criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+div1.style.backgroundColor = 'green';
+
+// 15.Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+lista.lastChild.remove('nove');
+lista.lastChild.remove('dez');
